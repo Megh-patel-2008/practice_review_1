f5d5e4e9a0116f8306b3995ac84e9b7a893b0681
Megh-patel-2008 <m3ghpatel2008@gmail.com>
2025-11-04 20:52:41 -0500

    Initial sharing of project

---
+++ Practice_review.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class Practice_review here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Practice_review
+{
+    String name = "Megh";
+    Integer age = 17;
+    ArrayList<String>words = new ArrayList<>();
+    private int year;
+    public final double PRICE = 10.00;
+    
+}
